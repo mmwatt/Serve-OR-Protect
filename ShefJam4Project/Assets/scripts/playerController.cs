84adbbac763198d6f173f735d260f6b48f3c3554
3DSMatt <matthew.m.watt@gmail.com>
2017-03-25 15:44:00 +0000

    Added some movement, unfinished.

--- ShefJam4Project/Assets/scripts/playerController.cs
+++ ShefJam4Project/Assets/scripts/playerController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour {
+public class playerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject bullet;
+    
+    public float movementSpeed;
+    public float xMin, xMax, yMin, yMax;
+    private Rigidbody2D rbody2D;
+
+    public void Awake () {
+        rbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        float changeHorizontal = Input.GetAxis("Horizontal");
+        float changeVertical = Input.GetAxis("Vertical");
+
+        Vector2 newPos = new Vector2(changeHorizontal, changeVertical);
+        rbody2D.velocity = newPos * movementSpeed;
+
+        rbody2D.position = new Vector2(Mathf.Clamp(rbody2D.position.x, xMin, xMax), Mathf.Clamp(rbody2D.position.y, yMin, yMax));
+    }
+
+    public void shootBullet () {
+        Instantiate(bullet);
+    }
 }
