f192fd52e68fea5dd6017742cefbf92fdfff273b
TheWispy <will0humph@gmail.com>
2017-03-25 15:32:41 +0000

    Camera follows player with limits

--- ShefJam4Project/Assets/scripts/playerController.cs
+++ ShefJam4Project/Assets/scripts/playerController.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class script : MonoBehaviour {
+public class playerController : MonoBehaviour {
+	private GameObject mainCamera;
 
 	// Use this for initialization
 	void Start () {
-		
+		mainCamera = (GameObject) GameObject.FindWithTag ("Main Camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
 	}
 }
