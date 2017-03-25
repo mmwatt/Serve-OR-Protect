4a502514811717a9653cfb9f62b1bdf87913755c
TheWispy <will0humph@gmail.com>
2017-03-25 17:56:36 +0000

    Enemies track player

---
+++ ShefJam4Project/Assets/scripts/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+	public GameObject bullet;
+	private GameObject player;
+	public float movementSpeed;
+	public float xMin, xMax, yMin, yMax;
+	private Rigidbody2D rbody2D;
+
+	public void Start(){
+		rbody2D = GetComponent<Rigidbody2D>();
+
+	}
+
+	void FixedUpdate ()
+	{
+		
+	}
+
+	void Update () {
+		Transform target = GameObject.FindWithTag("Player").transform;
+		Vector3 direction = (transform.position - target.position);
+		float step = movementSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+
+	}
+
+	public void Shoot(){
+	}
+
+	public void shootBullet () {
+		Instantiate(bullet);
+	}
+}
