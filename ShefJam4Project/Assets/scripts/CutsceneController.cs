90bde9b1054ae2cd8b5f160d53ef9e16b4d68892
3DSMatt <matthew.m.watt@gmail.com>
2017-03-26 08:27:14 +0100

    started cutscene template

---
+++ ShefJam4Project/Assets/scripts/CutsceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneController : MonoBehaviour {
+    public GameObject o_choiceBad;
+    public GameObject o_choiceGood;
+    public GameObject o_mainText;
+    public GameObject o_player;
+    public GameObject o_otherChar;
+
+    private Button choiceBad;
+    private Button choiceGood;
+    private Text mainText;
+    private Image player;
+    private Image otherChar;
+
+    private void Start () {
+        choiceBad = o_choiceBad.GetComponent<Button>();
+        choiceGood = o_choiceGood.GetComponent<Button>();
+        mainText = o_mainText.GetComponent<Text>();
+        player = o_player.GetComponent<Image>();
+        otherChar = o_otherChar.GetComponent<Image>();
+    }
+}
